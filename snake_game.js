254a4aac4ed0e6f757f41259739048fef7a2448d
Minhphuong2109 <LegendaryS9999@gmail.com>
2019-08-21 21:22:05 +0700

    comment

--- snake_game.js
+++ snake_game.js
@@ -1,9 +1,25 @@
 let gameLoop;
+let eatSound;
+let deadSound;
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+    }    
+}
+
 window.onload=function() {
     canv=document.getElementById("gc");
     ctx=canv.getContext("2d");
     document.addEventListener("keydown",keyPush);
-    
     gameLoop = setInterval(game,speed);
     console.log(level)
 }
@@ -17,11 +33,14 @@ function init(){
     xv=yv=0;
     trail=[];
     tail = 5;
-    speed = 100
+    speed = 100; 
+
 }
 init()
 
 function game() {
+    eatSound = new sound("bounce.mp3");
+    deadSound = new sound("dead.mp3")
     if (point > 3){
         level = Math.floor(point/3)
        
@@ -47,10 +66,9 @@ function game() {
     for(var i=0;i<trail.length;i++) {
         ctx.fillRect(trail[i].x*gs,trail[i].y*gs,gs-2,gs-2);
         if(trail[i].x==px && trail[i].y==py) {
-            tail = 3;
-            point = 0;
-            level = 1;
-            die = true
+            deadSound.stop();
+            deadSound.play();
+           
             init()
         }
     }
@@ -60,6 +78,9 @@ function game() {
     }
  
     if(ax==px && ay==py) {
+        eatSound.stop();
+        eatSound.play();
+        
         point ++;
         tail++;
 
@@ -70,12 +91,17 @@ function game() {
         }
         ax=Math.floor(Math.random()*tc);
         ay=Math.floor(Math.random()*tc);
+        for (var index = 0; index < trail.length; index++) {
+            if (trail[index].x==ax && trail[index].y==ay) {
+                ax=Math.floor(Math.random()*tc);
+                ay=Math.floor(Math.random()*tc);
+            }
+        }
     }
     ctx.fillStyle="red";
     ctx.fillRect(ax*gs,ay*gs,gs-2,gs-2);
     document.getElementById("score").innerHTML = "Point:"+ point
     document.getElementById("level").innerHTML = "Level:"+ level
-    // document.getElementById("status").innerHTML = Status + "Playing"
     
     
 }
